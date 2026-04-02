74c7b879b8d7068466421a9043eaa26fe7f76fe4
Artem <a.miheev@tgrad.ru>
2026-04-02 15:21:25 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -28,25 +28,30 @@ function App() {
   const webcamRef = useRef<Webcam | null>(null)
   const [frontCamera, setFrontCamera] = useState<boolean>(true)
   const intervalRef = useRef<number | null>(null)
-  const timeoutRef = useRef<number | null>(null)
-  const timeoutSeconds = defaultDelay * 1000
   const [timer, setTimer] = useState<number>(defaultDelay)
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
 
   const startTimer = async () => {
     intervalRef.current = setInterval(() => {
-      setTimer((prev) => prev - 1)
+      setTimer((prev) => {
+        if (prev <= 1) {
+          clearInterval(intervalRef.current!)
+
+          setTimeout(() => {
+            startFaceId()
+          }, 0)
+
+          return 0
+        }
+
+        return prev - 1
+      })
     }, 1000)
-    timeoutRef.current = setTimeout(() => {
-      clearInterval(intervalRef.current!)
-      startFaceId()
-    }, timeoutSeconds)
   }
 
   const startFaceId = () => {
     setTimeout(() => {
-      // window.alert("Запрос выполнен")
       setIsSubmitting(false)
       setTimer(defaultDelay)
     }, 3000)
@@ -104,39 +109,6 @@ function App() {
           <div>это мобилка: {isMobile ? "Да" : "Нет"}</div>
           <div>{navigator.userAgent}</div>
         </div>}
-      {/* 
-      <Webcam
-        className="mt-2"
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        style={{
-          borderRadius: "50%",
-        }}
-        mirrored={isMobile
-          ? frontCamera
-          : true}
-        width={300}
-        height={300}
-        onUserMediaError={() => {
-          window.alert("не получили девайсы")
-        }}
-        videoConstraints={{
-          facingMode: frontCamera ? "user" : "environment",
-          width: 300,
-          height: 300,
-        }}
-      />
-      <div className="mb-2 d-flex align-items-center">
-        <button className="ms-2 check-label" onClick={() => {
-          setFrontCamera(!frontCamera)
-        }}>
-          {frontCamera ? "включить переднюю камеру" : "включить фронтальную камеру"}
-        </button>
-      </div>
-      <button>Сделать запрос</button>
-      <div>это мобилка: {isMobile ? "Да" : "Нет"}</div>
-      <div>{navigator.userAgent}</div> */}
     </>
   );
 }
